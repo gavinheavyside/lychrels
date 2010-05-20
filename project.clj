51a35b1aae6cc9cff47a49bad2cbf005ae5b3ccf
Gavin Heavyside <gavin.heavyside@journeydynamics.com>
2010-05-20 14:11:35 +0100

    latest swank-clojure

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Lychrels problem from SCGUK coding dojo"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
   :main lychrels.core)
